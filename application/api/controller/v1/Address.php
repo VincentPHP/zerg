7eca6f55fc80b478724081cd4ebbbb2487f462e0
Vincent <admin@gzibm.com>
2017-07-22 23:31:40 +0800

    添加用户地址控制器

---
+++ application/api/controller/v1/Address.php
@@ -0,0 +1,69 @@
+<?php
+/* +----------------------------------------------------------------
+ * | Software: [AQPHP framework]
+ * |	 Site: www.aqphp.com
+ * |----------------------------------------------------------------
+ * |   Author: 赵 港 < admin@gzibm.com | 847623251@qq.com >
+ * |   WeChat: GZIDCW
+ * |   Copyright (C) 2015-2020, www.aqphp.com All Rights Reserved.
+ * +----------------------------------------------------------------*/
+
+namespace app\api\controller\v1;
+
+use app\api\service\Token as TokenService;
+use app\api\validate\AddressNew;
+use app\api\model\User as UserModel;
+use app\lib\success\SuccessMessage;
+use app\lib\exception\UserException;
+
+class Address
+{
+    /**
+     * 创建或更新用户地址
+     * @return SuccessMessage
+     * @throws UserException
+     */
+    public function createOrUpdateAddress()
+    {
+        //根据Token获取用户UID
+        //根据用户UID来查找用户数据，判断用户是否存在，如果不存在抛出异常
+        //获取客户从客户端提交的地址信息
+        //根据用户地址信息是否存在，从而判断是添加地址还是更新地址
+
+        //验证数据
+        $validate = new AddressNew();
+
+        $validate->goCheck();
+
+        //获取用户ID
+        $uid  = TokenService::getCurrentUid();
+
+        //获取用户信息
+        $user = UserModel::get($uid);
+
+        if(!$user)
+        {
+            //抛出异常
+            throw new UserException();
+        }
+
+        //用户提交的数据
+        $dataArray = $validate->getDataByRule(input('post.'));
+
+        //获取到的用户地址
+        $userAddress = $user->address;
+
+        if(!$userAddress)
+        {
+            //新增用户地址
+            $user->address()->save($dataArray);
+        }
+        else
+        {
+            //更新用户地址
+            $user->address->save($dataArray);
+        }
+
+        return new SuccessMessage();
+    }
+}
