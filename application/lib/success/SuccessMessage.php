4631c1b13426cd93b1d84e1aeb26462dea9bc976
Vincent <admin@gzibm.com>
2017-07-22 23:37:47 +0800

    新增成功通知类

---
+++ application/lib/success/SuccessMessage.php
@@ -0,0 +1,27 @@
+<?php
+/* +----------------------------------------------------------------
+ * | Software: [AQPHP framework]
+ * |	 Site: www.aqphp.com
+ * |----------------------------------------------------------------
+ * |   Author: 赵 港 < admin@gzibm.com | 847623251@qq.com >
+ * |   WeChat: GZIDCW
+ * |   Copyright (C) 2015-2020, www.aqphp.com All Rights Reserved.
+ * +----------------------------------------------------------------*/
+
+namespace app\lib\success;
+
+/**
+ * SuccessMessage 成功提示类
+ * @package app\lib\success
+ */
+class SuccessMessage
+{
+    //状态码
+    public $code = 201;
+
+    //成功提示信息
+    public $msg  = 'ok';
+
+    //错误码
+    public $errorCode = 0;
+}
