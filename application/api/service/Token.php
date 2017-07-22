db835e20400aa36a6973b0c1fcbb7035b99ef8e4
Vincent <admin@gzibm.com>
2017-07-22 23:34:51 +0800

    在Token中添加了获取缓存数据、获取用户ID等方法

--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -10,6 +10,10 @@
 
 namespace app\api\service;
 
+use app\lib\exception\TokenException;
+use think\Cache;
+use think\Request;
+
 /**
  * Token Service基类
  * @package app\api\service
@@ -34,5 +38,53 @@ class Token
         $salt = config('secure.token_salt');
 
         return md5($randChar.$timeStamp.$salt);
+    }
+
+
+    /**
+     * 从缓存中获取某个值
+     * @param $key
+     * @return mixed
+     * @throws TokenException
+     */
+    public static function getCurrentTokenVar($key)
+    {
+        //从Header中获取Token
+        $token = Request::instance()->header('token');
+
+        //获取缓存信息
+        $vars = Cache::get($token);
+
+        //抛出异常
+        if(!$vars)
+        {
+            throw new TokenException();
+        }
+        else
+        {
+            if(!is_array($vars))
+            {
+                //转换JSON格式为数组
+                $vars = json_decode($vars, true);
+            }
+
+            //如果缓存里不存在需要的Key 抛出异常
+            if(array_key_exists($key, $vars))
+            {
+                return $vars[$key];
+            }
+            else
+            {
+                throw new Exception('尝试获取的Token变量并不存在');
+            }
+        }
+    }
+
+
+
+    public static function getCurrentUid()
+    {
+        //获取Token
+        return self::getCurrentTokenVar('uid');
     }
 }
