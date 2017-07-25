48155ef41a061379e1ba2cc4d31bdeeb9def0955
Vincent <admin@gzibm.com>
2017-07-25 15:32:29 +0800

    模仿枚举类型做权限域

---
+++ application/lib/enum/ScopeEnum.php
@@ -0,0 +1,24 @@
+<?php
+/* +----------------------------------------------------------------
+ * | Software: [AQPHP framework]
+ * |	 Site: www.aqphp.com
+ * |----------------------------------------------------------------
+ * |   Author: 赵 港 < admin@gzibm.com | 847623251@qq.com >
+ * |   WeChat: GZIDCW
+ * |   Copyright (C) 2015-2020, www.aqphp.com All Rights Reserved.
+ * +----------------------------------------------------------------*/
+
+namespace app\lib\enum;
+
+/**
+ * ScopeEnum 模拟枚举类型 作用域
+ * @package app\lib\enum
+ */
+class ScopeEnum
+{
+    //作用域 App
+    const User = 16;
+
+    //作用域 CMS
+    const Super = 32;
+}
