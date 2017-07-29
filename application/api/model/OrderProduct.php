036b68ecbec502f6de8e3853a56ec69a10b8d169
Vincent <admin@gzibm.com>
2017-07-29 23:26:40 +0800

    新增OrderProduct模型

---
+++ application/api/model/OrderProduct.php
@@ -0,0 +1,20 @@
+<?php
+/* +----------------------------------------------------------------
+ * | Software: [AQPHP framework]
+ * |	 Site: www.aqphp.com
+ * |----------------------------------------------------------------
+ * |   Author: 赵 港 < admin@gzibm.com | 847623251@qq.com >
+ * |   WeChat: GZIDCW
+ * |   Copyright (C) 2015-2020, www.aqphp.com All Rights Reserved.
+ * +----------------------------------------------------------------*/
+
+namespace app\api\model;
+
+/**
+ * OrderProduct 模型
+ * @package app\api\model
+ */
+class OrderProduct extends BaseModel
+{
+
+}
