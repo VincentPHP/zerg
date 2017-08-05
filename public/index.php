6ef5a90721ff3923b1550586d8ed585336f752d3
Vincent <admin@gzibm.com>
2017-08-05 08:37:04 +0800

    删除入口文件的设置时间戳函数

--- public/index.php
+++ public/index.php
@@ -17,8 +17,6 @@ define('APP_PATH', __DIR__ . '/../application/');
 // 定义应用目录
 define('LOG_PATH', __DIR__ . '/../log/');
 
-date_default_timezone_set("PRC");
-
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
 
